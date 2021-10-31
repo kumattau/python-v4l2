161c080115d3607f1966221068b4c93dbd911e60
kumattau <kumattau@gmail.com>
2021-11-01 04:02:44 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# python-v4l2
+
+A python ctypes binding for video4linux2
+
+This binding contains the definitions
+in the following headers of linux-libc-dev:amd64 5.4.0-81.91.
+
+* asm-generic/ioctl.h
+* linux/time.h
+* linux/v4l2-common.h
+* linux/v4l2-controls.h
+* linux/videodev2.h
+
+
+## Example
+
+See [example.py](example.py).
+
+
+## License
+
+Copyright (c) 2021 kumattau
+
+This project is released under the [MIT Lisence](LICENSE).
---
+++ example.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2021 kumattau
+#
+# Use of this source code is governed by a MIT License
+#
+
+"""
+video capture example using python-v4l2
+"""
+
+import fcntl
+import mmap
+import selectors
+import sys
+
+import cv2
+import numba.cuda
+import numpy as np
+
+import v4l2
+
+
+def main():
+    DEVICE = "/dev/video0"
+    BUF_TYPE = v4l2.V4L2_BUF_TYPE_VIDEO_CAPTURE
+    FMT_TYPE = v4l2.V4L2_PIX_FMT_MPEG
+    WIDTH = 1280
+    HEIGHT = 720
+    REQ_COUNT = 4
+
+    if 1 < len(sys.argv) and sys.argv[1] == "--userptr":
+        MEM_TYPE = v4l2.V4L2_MEMORY_USERPTR
+        mode = "USERPTR"
+    else:
+        MEM_TYPE = v4l2.V4L2_MEMORY_MMAP
+        mode = "MMAP"
+    print(f"{sys.argv[0]}: Start capture with {mode} mode. Press Ctrl+C to exit")
+
+    fd = open(DEVICE, "rb+", buffering=0)
+
+    fmt = v4l2.v4l2_format()
+    fmt.type = BUF_TYPE
+    fmt.fmt.pix.width = WIDTH
+    fmt.fmt.pix.height = HEIGHT
+    fmt.fmt.pix.pixelformat = FMT_TYPE
+    fcntl.ioctl(fd, v4l2.VIDIOC_S_FMT, fmt)
+
+    req = v4l2.v4l2_requestbuffers()
+    req.type = BUF_TYPE
+    req.count = REQ_COUNT
+    req.memory = MEM_TYPE
+    fcntl.ioctl(fd, v4l2.VIDIOC_REQBUFS, req)
+
+    mems = []
+    for i in range(req.count):
+        buf = v4l2.v4l2_buffer()
+        buf.type = BUF_TYPE
+        buf.memory = MEM_TYPE
+        buf.index = i
+        fcntl.ioctl(fd, v4l2.VIDIOC_QUERYBUF, buf)
+
+        if MEM_TYPE == v4l2.V4L2_MEMORY_USERPTR:
+            arr = numba.cuda.pinned_array((buf.length,), dtype="uint8")
+            buf.m.userptr = arr.__array_interface__["data"][0]
+        else:
+            mem = mmap.mmap(
+                fd.fileno(), buf.length,
+                flags=mmap.MAP_SHARED, prot=mmap.PROT_READ, offset=buf.m.offset)
+            arr = np.frombuffer(mem, dtype="uint8")
+
+        mems.append(arr)
+        fcntl.ioctl(fd, v4l2.VIDIOC_QBUF, buf)
+
+    selector = selectors.DefaultSelector()
+    selector.register(fd, selectors.EVENT_READ)
+    # convert bytes to pass int *, not int
+    fcntl.ioctl(fd, v4l2.VIDIOC_STREAMON, BUF_TYPE.to_bytes(8, "little"))
+
+    while True:
+        _ = selector.select()
+
+        buf = v4l2.v4l2_buffer()
+        buf.type = BUF_TYPE
+        buf.memory = MEM_TYPE
+        fcntl.ioctl(fd, v4l2.VIDIOC_DQBUF, buf)
+
+        arr = mems[buf.index]
+        img = cv2.imdecode(arr, cv2.IMREAD_UNCHANGED)
+        fcntl.ioctl(fd, v4l2.VIDIOC_QBUF, buf)
+
+        cv2.imshow(DEVICE, img)
+        key = cv2.waitKey(1)
+        if key == 99:
+            break
+
+
+if __name__ == "__main__":
+    main()
